52884a5a280196fb203c54df450830cd20a99dc6
eeyc88 <erick-yepez-cedeno@hotmail.com>
2017-09-25 01:02:23 -0500

    modificacion del index

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ session_start();
 	<title>Formulario</title>
 	<meta name= "viewport" content= "width = device-width, initial-escale=1.0">
 	<meta http-equiv= "Content-Type" content= "text/html; charset=UTF-8" />
-	<link rel= "stylesheet" type="text/css" href="../css/bootstrap.css">
-	<link rel= "stylesheet" type="text/css" href="../css/login.css">
+	<link rel= "stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel= "stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
 	<div class="jumbotron boxlogin" >
